9043a557aa4eea767a4c9c67fdc84bbbad2865a8
mrasamny <mrasamny@desu.edu>
2023-12-08 21:07:09 -0500

    first commit

---
+++ Controller.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Controller here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Controller extends World
+{
+    /**
+     * Constructor for objects of class Controller.
+     * 
+     */
+    public Controller()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 650, 1); 
+    }
+}
